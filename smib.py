e2c4cb45761e881640563abbb223f5d43d1d80ac
Yom Rawr <yomanda@gmail.com>
2016-07-04 20:12:16 +0100

    Update smib.py
    try ignore some exceptions

--- smib.py
+++ smib.py
@@ -7,12 +7,16 @@ import re
 import subprocess
 from slackclient import SlackClient
 
-token = "do_not_check_in"
+token = "xoxb-316470992"
 bot_user = "U0XK12X63"
 
 programsdir = '/home/smib/smib-commands/'
 all_commands = {}
 all_commands_time = 0
+
+error_count = 0
+users = {}
+chans = {}
 
 sc = SlackClient(token)
 
@@ -27,7 +31,7 @@ def get_commands_by_dir():
                 match = re.search(r"(\w+).\w+", filename, re.MULTILINE)
                 if match:
                     all_commands[match.group(1)] = programsdir + filename
-                    print programsdir + filename
+                    #print programsdir + filename
         all_commands_time = d
     return
 
@@ -41,27 +45,78 @@ def get_command(command):
     # todo: partial matches
 
 if sc.rtm_connect():
+    # initial poll of smib-commands
     get_commands_by_dir()
+    
+    # initial poll of users
+    userslist = sc.api_call("users.list")
+    for member in userslist['members']:
+        users[member["id"]] = member["name"]
+    
+    chanslist = sc.api_call("channels.list")
+    for channel in chanslist['channels']:
+        chans[channel["id"]] = channel["name"]
+    
     while True:
-        new_evts = sc.rtm_read()
+        try:
+            new_evts = sc.rtm_read()
+        except:
+            if error_count > 10:
+                raise
+            error_count += 1
+            continue
+        
+        error_count = 0
         for evt in new_evts:
             #print evt
-            if "type" in evt and "user" in evt:
-                if evt["type"] == "message" and evt["user"] != bot_user and "text" in evt:
-                    match = re.search(r"^\?(\w+) {0,1}(.*)", evt["text"], re.MULTILINE)
+            if "type" in evt:
+                if evt["type"] == "message" and "text" in evt:
+                    try:
+                        match = re.search(r"^\?(\w+) {0,1}(.*)", evt["text"], re.MULTILINE)
+                    except:
+                        print evt
+                        continue
                     if match:
-                        sc.server.send_to_websocket({"type": "typing", "channel": str(evt["channel"]), "id": 1})
+                        sc.server.send_to_websocket({"type": "typing", "channel": evt["channel"], "id": 1})
                         get_commands_by_dir()
                         command = match.group(1).lower()
                         argline = match.group(2)
                         is_command = get_command(command)
                         if is_command == False:
-                            print sc.api_call("chat.postMessage", as_user="false:", channel=evt["channel"], text="Sorry, I don't have a "+command+" command.")
+                            sc.api_call("chat.postMessage", as_user="false:", channel=evt["channel"], text="Sorry, I don't have a "+command+" command.")
                             continue
                         script = all_commands[command]
                         os.chdir(programsdir)
-                        p = subprocess.Popen(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                        print sc.api_call("chat.postMessage", as_user="false:", channel=evt["channel"], text=''.join(p.stdout.readlines()))
-                    time.sleep(3)
+                        
+                        # Channel Messages
+                        if evt["channel"][:1] == "C":
+                            try:
+                                p = subprocess.Popen([script, users[evt["user"]], chans[evt["channel"]], chans[evt["channel"]], argline], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                                sc.api_call("chat.postMessage", as_user="false:", channel=evt["channel"], text=''.join(p.stdout.readlines()))
+                            except:
+                                sc.api_call("chat.postMessage", as_user="false:", channel=evt["channel"], text=command+" is on fire!")
+                            
+                        # Direct Messages
+                        if evt["channel"][:1] == "D":
+                            try:
+                                p = subprocess.Popen([script, users[evt["user"]], 'null', evt["channel"], argline], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                                sc.api_call("chat.postMessage", as_user="false:", channel=evt["channel"], text=''.join(p.stdout.readlines()))
+                            except:
+                                sc.api_call("chat.postMessage", as_user="false:", channel=evt["channel"], text=command+" is on fire!")
+                            
+                    time.sleep(1)
+                    continue
+                if evt["type"] == "user_change":
+                    userslist = sc.api_call("users.list")
+                    for member in userslist['members']:
+                        users[member["id"]] = member["name"]
+                    continue
+                if evt["type"] == "channel_created" or evt["type"] == "channel_rename":
+                    chanslist = sc.api_call("channels.list")
+                    for channel in chanslist['channels']:
+                        chans[channel["id"]] = channel["name"]
+                
+                    
+        time.sleep(0.1)
 else:
     print "Connection Failed, invalid token?"
